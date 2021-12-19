2b1983b338f61257bf1fa4578cdfe0daae94ef73
lifegpc <g1710431395@gmail.com>
2021-12-19 15:15:02 +0800

    fuckzip package

---
+++ fuckzip/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/lifegpc/fuckzip
+TERMUX_PKG_DESCRIPTION="A tool that extract zip archive with a custom encoding"
+TERMUX_PKG_LICENSE="AGPL-3.0"
+TERMUX_PKG_MAINTAINER="lifegpc"
+TERMUX_PKG_VERSION=1.0.0
+TERMUX_PKG_SRCURL=https://github.com/lifegpc/fuckzip.git
+TERMUX_PKG_DEPENDS="libiconv, libzip"
+TERMUX_PKG_LICENSE_FILE="LICENSE"
+
