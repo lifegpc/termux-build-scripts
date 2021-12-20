c9e4e5cca2ae482d4e2b5fdcaba13944d2f8409b
lifegpc <g1710431395@gmail.com>
2021-12-20 14:29:12 +0800

    Add libtd-git

---
+++ libtd-git/build.sh
@@ -0,0 +1,28 @@
+TERMUX_PKG_HOMEPAGE=https://core.telegram.org/tdlib/
+TERMUX_PKG_DESCRIPTION="Library for building Telegram clients"
+TERMUX_PKG_LICENSE="BSL-1.0"
+TERMUX_PKG_REPLACES=libtd
+TERMUX_PKG_PROVIDES="tdlib, libtd"
+_COMMIT=4eaae3306829be9aa66f9dd2a7e3d3895e01841e
+TERMUX_PKG_VERSION="v1.7.0-1935-g${_COMMIT:0:8}"
+TERMUX_PKG_SRCURL="https://github.com/tdlib/td.git"
+TERMUX_PKG_GIT_BRANCH=master
+TERMUX_PKG_DEPENDS="readline, openssl (>= 1.1.1), zlib"
+TERMUX_PKG_HOSTBUILD=true
+
+termux_step_post_get_source() {
+    cd $TERMUX_PKG_SRCDIR
+    git checkout $_COMMIT
+}
+
+termux_step_host_build() {
+    termux_setup_cmake
+    cmake "-DCMAKE_BUILD_TYPE=Release" "$TERMUX_PKG_SRCDIR"
+    cmake --build . --target prepare_cross_compiling
+}
+
+termux_step_post_make_install() {
+    # Fix rebuilds without ./clean.sh.
+    rm -rf $TERMUX_PKG_HOSTBUILD_DIR
+}
+
