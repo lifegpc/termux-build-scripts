94ae8a8192b7aac95dacf3871d48d1ef164419d1
lifegpc <g1710431395@gmail.com>
2021-12-20 15:01:39 +0800

    remove v in version (shit apt)

--- libtd-git/build.sh
+++ libtd-git/build.sh
@@ -4,11 +4,12 @@ TERMUX_PKG_LICENSE="BSL-1.0"
 TERMUX_PKG_REPLACES=libtd
 TERMUX_PKG_PROVIDES="tdlib, libtd"
 _COMMIT=4eaae3306829be9aa66f9dd2a7e3d3895e01841e
-TERMUX_PKG_VERSION="v1.7.0-1935-g${_COMMIT:0:8}"
+TERMUX_PKG_VERSION="1.7.0-1935-g${_COMMIT:0:8}"
 TERMUX_PKG_SRCURL="https://github.com/tdlib/td.git"
 TERMUX_PKG_GIT_BRANCH=master
 TERMUX_PKG_DEPENDS="readline, openssl (>= 1.1.1), zlib"
 TERMUX_PKG_HOSTBUILD=true
+TERMUX_PKG_MAINTAINER=lifegpc
 
 termux_step_post_get_source() {
     cd $TERMUX_PKG_SRCDIR
