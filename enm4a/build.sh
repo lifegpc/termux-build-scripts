05be5da6ba8735262ba357464dc3e4f78e02a43c
lifegpc <g1710431395@gmail.com>
2021-12-21 11:02:31 +0800

    Add enm4a

---
+++ enm4a/build.sh
@@ -0,0 +1,13 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/lifegpc/ffmpeg-study
+TERMUX_PKG_DESCRIPTION="Convert file to m4a file."
+TERMUX_PKG_LICENSE="AGPL-3.0"
+TERMUX_PKG_VERSION=1.0.1.6
+TERMUX_PKG_SRCURL=https://github.com/lifegpc/ffmpeg-study.git
+TERMUX_PKG_GIT_BRANCH=master
+TERMUX_PKG_DEPENDS="ffmpeg"
+TERMUX_PKG_LICENSE_FILE="../LICENSE"
+
+termux_step_post_get_source() {
+    export "TERMUX_PKG_SRCDIR=$TERMUX_PKG_SRCDIR/enm4a"
+}
+
