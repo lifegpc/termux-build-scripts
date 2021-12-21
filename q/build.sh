aba1f365d21975026f85e209f150ff8234c3d79d
lifegpc <g1710431395@gmail.com>
2021-12-21 08:49:05 +0800

    Add q

---
+++ q/build.sh
@@ -0,0 +1,31 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/natesales/q
+TERMUX_PKG_DESCRIPTION="A tiny command line DNS client with support for UDP, DoT, DoH, DoQ and ODoH."
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_VERSION=0.4.2
+TERMUX_PKG_SRCURL=https://github.com/natesales/q.git
+TERMUX_PKG_LICENSE_FILE=LICENSE
+
+termux_step_make() {
+    cd $TERMUX_PKG_SRCDIR
+    case $TERMUX_ARCH in
+    aarch64)
+        GO_ARCH=arm64
+    ;;
+    i686)
+        GO_ARCH=386
+    ;;
+    x86_64)
+        GO_ARCH=amd64
+    ;;
+    *)
+        GO_ARCH=$TERMUX_ARCH
+    ;;
+    esac
+    env GOOS=android GOARCH=$GO_ARCH CGO_ENABLED=1 CC=$CC go build
+}
+
+termux_step_make_install() {
+    cd $TERMUX_PKG_SRCDIR
+    cp q $TERMUX_PREFIX/bin -v
+}
+
