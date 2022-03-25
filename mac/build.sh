6c90f451d32efbfd92ffd692c51afc2cb1704abf
lifegpc <g1710431395@gmail.com>
2022-03-25 15:39:07 +0800

    Bump mac from 7.22 to 7.53

--- mac/build.sh
+++ mac/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="Monkey's Audio is a fast and easy way to compress digita
 TERMUX_PKG_LICENSE="see https://monkeysaudio.com/license.html"
 TERMUX_PKG_LICENSE_FILE="Readme.txt"
 TERMUX_PKG_MAINTAINER="lifegpc"
-TERMUX_PKG_VERSION=7.22
-TERMUX_PKG_SRCURL=https://monkeysaudio.com/files/MAC_SDK_722.zip
-TERMUX_PKG_SHA256=2dae19002a744715254dbc61ebb3db88cabeb612dbdfed207dc2c921addff921
+TERMUX_PKG_VERSION=7.53
+TERMUX_PKG_SRCURL=https://monkeysaudio.com/files/MAC_SDK_753.zip
+TERMUX_PKG_SHA256=0fa9d0b04c8215621e0e12a40babda9e556d3f48e43af91891e4ed7f2f38b7d3
 
 termux_step_get_source() {
     curl -L "$TERMUX_PKG_SRCURL" -o tmp.zip || exit 1
