21abe0c55443a77132747ba22718a1f4f5d45ea1
lifegpc <g1710431395@gmail.com>
2022-04-03 18:22:16 +0800

    Add cdecl

---
+++ cdecl/build.sh
@@ -0,0 +1,11 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/paul-j-lucas/cdecl
+TERMUX_PKG_DESCRIPTION='Composing and deciphering C (or C++) declarations or casts, aka ‘‘gibberish.’’'
+TERMUX_PKG_LICENSE='GPL-3.0'
+TERMUX_PKG_LICENSE_FILE=COPYING
+TERMUX_PKG_MAINTAINER=lifegpc
+TERMUX_PKG_VERSION=11.14
+TERMUX_PKG_SRCURL=https://github.com/paul-j-lucas/cdecl/releases/download/cdecl-11.14/cdecl-11.14.tar.gz
+TERMUX_PKG_SHA256=b4ad44a417048db94d2bb9f34b3f83c37b5c7b9bd410f2a2ec89187e70dbc527
+TERMUX_PKG_DEPENDS='readline'
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS='--disable-cdecl-debug'
+
