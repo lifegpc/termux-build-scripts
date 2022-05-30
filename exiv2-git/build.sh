79ae231d681d5c75b6a225a0f532ba2c73d9f237
lifegpc <g1710431395@gmail.com>
2022-05-30 23:27:51 +0000

    Bump exiv2-git from 0.27-2261-g5ed9fb41 to 0.27-2408-g7ebf2a18

--- exiv2-git/build.sh
+++ exiv2-git/build.sh
@@ -2,8 +2,8 @@ TERMUX_PKG_HOMEPAGE=https://exiv2.org/
 TERMUX_PKG_DESCRIPTION="Exif, Iptc and XMP metadata manipulation library and tools"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="lifegpc"
-_COMMIT=5ed9fb4120c9dd376bb468403d9af2e30f3c2e64
-TERMUX_PKG_VERSION=0.27-2261-g${_COMMIT:0:8}
+_COMMIT=7ebf2a184e28bf638a39aa30ed3784c5035b668a
+TERMUX_PKG_VERSION=0.27-2408-g${_COMMIT:0:8}
 TERMUX_PKG_SRCURL=https://github.com/Exiv2/exiv2.git
 TERMUX_PKG_DEPENDS="libandroid-support, libc++, libexpat, zlib"
 TERMUX_PKG_GIT_BRANCH=main
@@ -16,6 +16,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 
 termux_step_post_get_source() {
     cd $TERMUX_PKG_SRCDIR
+    git pull
     git checkout $_COMMIT
 }
 
