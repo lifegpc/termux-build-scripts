5bc2f0f64cc55c11807d1cdcc96d538c88c1e3ef
lifegpc <g1710431395@gmail.com>
2022-05-30 23:19:22 +0000

    Bump q from 0.4.4 to 0.6.0

--- q/build.sh
+++ q/build.sh
@@ -1,9 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/natesales/q
 TERMUX_PKG_DESCRIPTION="A tiny command line DNS client with support for UDP, DoT, DoH, DoQ and ODoH."
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=0.4.4
-TERMUX_PKG_SRCURL=https://github.com/natesales/q.git
+TERMUX_PKG_VERSION=0.6.0
+TERMUX_PKG_SRCURL=https://github.com/natesales/q/archive/refs/tags/v0.6.0.tar.gz
 TERMUX_PKG_LICENSE_FILE=LICENSE
+TERMUX_PKG_SHA256=3e611ad53c441744c60e0b522207b8f5c3fe0ebde35f2289533436b310de68f7
 
 termux_step_make() {
     cd $TERMUX_PKG_SRCDIR
