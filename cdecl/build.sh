d8a7851ac5001bbf358eb3f58c9c10cd841c1cfa
lifegpc <g1710431395@gmail.com>
2022-05-30 15:26:37 +0000

    Bump cdecl from 11.14 to 11.15

--- cdecl/build.sh
+++ cdecl/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION='Composing and deciphering C (or C++) declarations or cas
 TERMUX_PKG_LICENSE='GPL-3.0'
 TERMUX_PKG_LICENSE_FILE=COPYING
 TERMUX_PKG_MAINTAINER=lifegpc
-TERMUX_PKG_VERSION=11.14
-TERMUX_PKG_SRCURL=https://github.com/paul-j-lucas/cdecl/releases/download/cdecl-11.14/cdecl-11.14.tar.gz
-TERMUX_PKG_SHA256=b4ad44a417048db94d2bb9f34b3f83c37b5c7b9bd410f2a2ec89187e70dbc527
+TERMUX_PKG_VERSION=11.15
+TERMUX_PKG_SRCURL=https://github.com/paul-j-lucas/cdecl/releases/download/cdecl-11.15/cdecl-11.15.tar.gz
+TERMUX_PKG_SHA256=548d9daba9202b660223fce5f78c2ba0c0886d7c51ab2a008e0a758812a41c06
 TERMUX_PKG_DEPENDS='readline'
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS='--disable-cdecl-debug'
 
