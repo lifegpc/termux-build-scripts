277f54dee7c7bb823d3294dbfdd72870e0d8ff50
lifegpc <g1710431395@gmail.com>
2022-06-13 10:30:52 +0000

    Bump mac from 8.08 to 8.17

--- mac/build.sh
+++ mac/build.sh
@@ -4,9 +4,9 @@ TERMUX_PKG_LICENSE="see https://monkeysaudio.com/license.html"
 TERMUX_PKG_LICENSE_FILE="Readme.txt"
 TERMUX_PKG_MAINTAINER="lifegpc"
 TERMUX_PKG_DEPENDS="libc++"
-TERMUX_PKG_VERSION=8.08
-TERMUX_PKG_SRCURL=https://monkeysaudio.com/files/MAC_SDK_808.zip
-TERMUX_PKG_SHA256=7976621cdb8d3701e98fa7d18df7e44da9c37e46c7b712c2e7b47b5605d875dd
+TERMUX_PKG_VERSION=8.17
+TERMUX_PKG_SRCURL=https://monkeysaudio.com/files/MAC_SDK_817.zip
+TERMUX_PKG_SHA256=1c7fb2469ff57056bd15581cbe4847226f26d4ad7e731a7ab4ad7ce97ed5e03e
 
 termux_step_get_source() {
     curl -L "$TERMUX_PKG_SRCURL" -o tmp.zip || exit 1
