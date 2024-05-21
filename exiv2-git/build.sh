75a7c1136f4954c87d03722bbc055cb793b6ffcf
lifegpc <root@lifegpc.com>
2024-05-21 09:54:43 +0000

    Bump exiv2-git

--- exiv2-git/build.sh
+++ exiv2-git/build.sh
@@ -4,7 +4,7 @@ TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="lifegpc"
 _COMMIT=7ebf2a184e28bf638a39aa30ed3784c5035b668a
 TERMUX_PKG_VERSION=0.27-2408-g${_COMMIT:0:8}
-TERMUX_PKG_SRCURL=https://github.com/Exiv2/exiv2.git
+TERMUX_PKG_SRCURL=git+https://github.com/Exiv2/exiv2
 TERMUX_PKG_DEPENDS="libandroid-support, libc++, libexpat, zlib"
 TERMUX_PKG_GIT_BRANCH=main
 TERMUX_PKG_BREAKS="exiv2"
@@ -15,8 +15,14 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DEXIV2_ENABLE_BUILD_SAMPLES=OFF"
 
 termux_step_post_get_source() {
+    local TMP_CHECKOUT=$TERMUX_PKG_CACHEDIR/tmp-checkout
+    rm -rf $TMP_CHECKOUT
+    git clone --branch "$TERMUX_PKG_GIT_BRANCH" "${TERMUX_PKG_SRCURL:4}" $TMP_CHECKOUT
+    cd $TMP_CHECKOUT
+    git submodule update --init --recursive
+    rm -rf $TERMUX_PKG_SRCDIR
+	cp -Rf $TMP_CHECKOUT $TERMUX_PKG_SRCDIR
     cd $TERMUX_PKG_SRCDIR
-    git pull
     git checkout $_COMMIT
 }
 
